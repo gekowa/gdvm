ee0708aea9df47f532e6edd47112745fb8e3231d
gekowa <gekowa@gmail.com>
2013-11-23 17:47:29 +0800

    输出未捕捉异常

--- main.js
+++ main.js
@@ -123,5 +123,7 @@ if (program.mode === "D") {
 }
 
 process.on('uncaughtException', function(err) {
-	logger.error(err);
+	console.error((new Date()).toUTCString() + ' uncaughtException:', err.message);
+	console.error(err.stack);
+	process.exit(1);
 });
